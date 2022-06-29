a9c16d195a3529d63af18eae90eb028808d5bafe
Sreerag <sreerag.rajan@hotmail.com>
2022-06-29 22:43:42 +0530

    Navbar added and logout also added

--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -1,12 +1,39 @@
+import { Button } from "@mui/material"
+import { useDispatch, useSelector } from "react-redux"
 import { Link } from "react-router-dom"
+import { setUser } from "../../Redux/Auth/auth.action"
+import './Navbar.css'
 
 
 export const Navbar = ()=>{
+    const {user} = useSelector(store=>store.auth);
+    const dispatch = useDispatch()
+
+    const handleLogout = ()=>{
+        localStorage.removeItem('__tk_user');
+        localStorage.removeItem('__tk_userToken');
+        dispatch(setUser(null));        
+    }
     return(
         <nav>
+            <div>
+            <Link to="/">Home</Link>
             <Link to="/dashboard">Dashboard</Link>
-            <Link to="/login">Login</Link>
-            <Link to="/signUp">SignUp</Link>
+            </div>
+            {!user?<div>
+            <Link to="/login">
+                <Button variant="outlined" sx={{color:'green', border: '1px solid green'}} >
+                Login
+                </Button>
+            </Link>
+            <Link to="/signUp">
+                <Button variant="contained" >
+                SignUp
+                </Button>
+            </Link>
+            </div>:
+            <Button onClick={handleLogout} variant="contained" sx={{backgroundColor: "red"}}>LogOut</Button>
+            }
             
         </nav>
     )
